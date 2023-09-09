9aaadda914d26a48034779d4c6700427b727a1e5
Jonas-Marvin Lühr <95620676+JonasMarvin@users.noreply.github.com>
2023-09-09 13:49:03 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -14,8 +14,8 @@ project "GLFW"
       		"src/input.c",
       		"src/monitor.c",
       		"src/vulkan.c",
-      		"src/window.c"
-		"src/internal.h"
+      		"src/window.c",
+		"src/internal.h",
 		"src/platform.c",
 		"src/null_init.c",
 		"src/null_monitor.c",
@@ -33,7 +33,7 @@ project "GLFW"
 		        "src/win32_joystick.c",
 		        "src/win32_monitor.c",
 			"src/win32_time.c",
-			"src/win32_module.c"
+			"src/win32_module.c",
 		        "src/win32_thread.c",
 		        "src/win32_window.c",
 		        "src/wgl_context.c",
